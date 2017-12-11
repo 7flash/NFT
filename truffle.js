de5fdcd87b1a760f7cc3934eedea91c4e6753ffd
Nadav Hollander <nadav@dharma.io>
2017-12-10 21:11:21 -0800

    init

---
+++ truffle.js
@@ -0,0 +1,18 @@
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id,
+      gas: 4712388,
+    },
+    kovan: {
+      host: 'localhost',
+      port: 8546,
+      network_id: '42',
+      gas: 4712388,
+    },
+  },
+  test_directory: 'transpiled/test',
+  migrations_directory: 'transpiled/migrations',
+};
